be0dedccf82fded1eec5091e9fdc5575af20bf0b
lpoliveiraneto <leonidespires@gmail.com>
2020-12-22 11:52:08 -0300

    aula 2 do curso 2

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,15 +2,31 @@ import React, { useState } from "react";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 import DadosEntrega from "./DadosEntrega";
+import { Typography } from "@material-ui/core";
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
+  const[etapaAtual, setEtapaAtual] = useState(0);
+
+  function formularioAtual(etapa){
+    switch(etapa){
+      case 0:
+        return <DadosUsuario />
+      case 1:
+        return <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>
+      case 2: 
+        return <DadosEntrega />
+      default:
+        return <Typography>Erro ao selecionar o formulario</Typography>
+    }
+  }
+
   return (
     <>
-      <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
-      <DadosUsuario />
-      <DadosEntrega />
+     {formularioAtual(etapaAtual)}
     </>
   );
 }
+
+
 
 export default FormularioCadastro;
