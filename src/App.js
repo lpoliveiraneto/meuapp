3e25775a0aa740dd5e2ee5e7b7f9ee1322f43b8c
lpoliveiraneto <leonidespires@gmail.com>
2021-01-15 11:38:25 -0300

    finalização do projeto sem os test

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ function App() {
   return (
     <Container component="article" maxWidth="sm">
       <Typography variant="h3" component="h1" align="center">Formulário de Cadastro</Typography>
-      <ValidacoesCadastro.Provider value={{cpf:validarCPF, senha:validarSenha, nome:validarSenha}}>
+      <ValidacoesCadastro.Provider 
+        value={{cpf:validarCPF, senha:validarSenha, nome:validarSenha}}
+      >
        <FormularioCadastro aoEnviar={aoEnviarForm} />
       </ValidacoesCadastro.Provider>
       
